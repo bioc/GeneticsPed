28d4933f3fc76302dcd9549524deedcc0dc32888
Marc Carlson <mcarlson@fhcrc.org>
2014-03-18 23:13:35 +0000

    From Dave: "Code improperly assumed that the order of ascendants was set to male then female. If this was not the case, then the variable tmp was often not set and the code would fail. Additionally, tmp was not reset between runs which could result in inflation of the relationship coefficients. This version should fix the problem...."
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GeneticsPed@87589 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/relationshipAdditive.R
+++ R/relationshipAdditive.R
@@ -8,49 +8,101 @@
 ### {{{ relationshipAdditive
 ###------------------------------------------------------------------------
 
-relationshipAdditive <- function(x, sort=TRUE, names=TRUE, ...)
-{
-  ## --- Setup ---
+## relationshipAdditive <- function(x, sort=TRUE, names=TRUE, ...)
+## {
+##   ## --- Setup ---
 
-  subject <- attr(x, ".subject")
-  ascendant <- attr(x, ".ascendant")
-  if(length(ascendant) > 2)
-      stop("no method for pedigree with more than two ascendants")
-  if(sort) idOrig <- as.character(x[[subject]])  # for sorting-back
+##   subject <- attr(x, ".subject")
+##   ascendant <- attr(x, ".ascendant")
+##   if(length(ascendant) > 2)
+##       stop("no method for pedigree with more than two ascendants")
+##   if(sort) idOrig <- as.character(x[[subject]])  # for sorting-back
 
-  ## Pedigree must be sorted and extended
-  x <- GeneticsPed:::checkAttributes(x, sorted=TRUE, extended=TRUE)
+##   ## Pedigree must be sorted and extended
+##   x <- GeneticsPed:::checkAttributes(x, sorted=TRUE, extended=TRUE)
 
-  n <- nrow(x)
-  ret <- matrix(0, n, n)          # n*n matrix
-  diag(ret) <- 1                  # diagonal is at least 1
-  rownames(ret) <- colnames(ret) <- x[[subject]]
+##   n <- nrow(x)
+##   ret <- matrix(0, n, n)          # n*n matrix
+##   diag(ret) <- 1                  # diagonal is at least 1
+##   rownames(ret) <- colnames(ret) <- x[[subject]]
 
-  asc1 <- as.character(x[[ascendant[1]]])
-  asc2 <- as.character(x[[ascendant[2]]])
-  testAsc1 <- !isUnknown(x[[ascendant[1]]])
-  testAsc2 <- !isUnknown(x[[ascendant[2]]])
-  testAsc  <- testAsc1 & testAsc2
-  ## subjects with at least one ascendant known
-  set <- which(!(!testAsc1 & !testAsc2))
+##   asc1 <- as.character(x[[ascendant[1]]])
+##   asc2 <- as.character(x[[ascendant[2]]])
+##   testAsc1 <- !isUnknown(x[[ascendant[1]]])
+##   testAsc2 <- !isUnknown(x[[ascendant[2]]])
+##   testAsc  <- testAsc1 & testAsc2
+##   ## subjects with at least one ascendant known
+##   set <- which(!(!testAsc1 & !testAsc2))
 
-  ## --- Core ---
+##   ## --- Core ---
 
-  for(i in set) {
-    ## Diagonal
-    if(testAsc[i]) ret[i, i] <- 1 + 0.5 * ret[asc1[i], asc2[i]]
-    ## Off-diagonal
-    j <- 1:(i - 1)                # working with lower triangle
-    if(testAsc1[i]) tmp <-       0.5 * ret[asc1[i], j]
-    if(testAsc2[i]) tmp <- tmp + 0.5 * ret[asc2[i], j]
-    ret[i, j] <- ret[j, i] <- tmp
-  }
+##   for(i in set) {
+##     ## Diagonal
+##     if(testAsc[i]) ret[i, i] <- 1 + 0.5 * ret[asc1[i], asc2[i]]
+##     ## Off-diagonal
+##     j <- 1:(i - 1)                # working with lower triangle
+##     if(testAsc1[i]) tmp <-       0.5 * ret[asc1[i], j]
+##     if(testAsc2[i]) tmp <- tmp + 0.5 * ret[asc2[i], j]
+##     ret[i, j] <- ret[j, i] <- tmp
+##   }
 
-  ## --- End ---
+##   ## --- End ---
 
-  if(sort) ret <- ret[idOrig, idOrig]
-  if(!names) attr(ret, which="dimnames") <- NULL
-  ret
+##   if(sort) ret <- ret[idOrig, idOrig]
+##   if(!names) attr(ret, which="dimnames") <- NULL
+##   ret
+## }
+
+relationshipAdditive <- function(x, sort=TRUE, names=TRUE, ...){
+    ## --- Setup ---
+    subject <- attr(x, ".subject")
+    ascendant <- attr(x, ".ascendant")
+    if(length(ascendant) > 2)
+        stop("no method for pedigree with more than two ascendants")
+    if(sort) idOrig <- as.character(x[[subject]]) # for sorting-back
+    ## Pedigree must be sorted and extended
+    x <- GeneticsPed:::checkAttributes(x, sorted=TRUE, extended=TRUE)
+    n <- nrow(x)
+    ret <- matrix(0, n, n) # n*n matrix
+    diag(ret) <- 1 # diagonal is at least 1
+    rownames(ret) <- colnames(ret) <- x[[subject]]
+    asc1 <- as.character(x[[ascendant[1]]])
+    asc2 <- as.character(x[[ascendant[2]]])
+    testAsc1 <- !isUnknown(x[[ascendant[1]]])
+    testAsc2 <- !isUnknown(x[[ascendant[2]]])
+    testAsc <- testAsc1 & testAsc2
+
+    ## subjects with at least one ascendant known
+    set <- which(!(!testAsc1 & !testAsc2))
+    ## --- Core ---
+    for(i in set) {
+        ## Diagonal
+        if(testAsc[i]) ret[i, i] <- 1 + 0.5 * ret[asc1[i], asc2[i]]
+        ## Off-diagonal
+        j <- 1:(i - 1) # working with lower triangle
+        if( testAsc1[i] )
+        {
+            tmp1 <- 0.5 * ret[asc1[i], j]
+        }
+        else
+        {
+            tmp1 <- 0
+        }
+        if( testAsc2[i] )
+        {
+            tmp2 <- 0.5 * ret[asc2[i], j]
+        }
+        else
+        {
+            tmp2 <- 0
+        }
+        ret[i, j] <- ret[j, i] <- tmp1 + tmp2
+    }
+
+    ## --- End ---
+    if(sort) ret <- ret[idOrig, idOrig]
+    if(!names) attr(ret, which="dimnames") <- NULL
+    ret
 }
 
 ### }}}
